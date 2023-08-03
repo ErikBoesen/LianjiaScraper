907086bf5080533fd4c2fa2c694fd0c301e16db1
ErikBoesen <me@erikboesen.com>
2023-08-03 13:50:57 -0400

    Support arbitrary number of pages

--- scrape.py
+++ scrape.py
@@ -13,21 +13,28 @@ print(cities)
 
 units = []
 for city_name, city_url in cities.items():
-    for page in range(1, 1 + 1):
+    page = 0
+    while True:
+        page += 1
         print(f'On {city_name} page {page}, now have found {len(units)} units.')
         response = requests.get(f'{city_url}ershoufang/pg{page}rs{city_name}/', cookies=cookies, headers=headers)
 
         soup = BeautifulSoup(response.text, 'html.parser')
         lis = soup.select('ul.sellListContent > li')
+        if len(lis) == 0:
+            print('No more units found on this page.')
+            break
         for li in lis:
-            unit = {}
+            unit = {
+                'city': city_name,
+            }
             infos = li.select('div.info.clear > div')
             for field in infos:
                 unit[field['class'][0]] = field.text.strip()
             units.append(unit)
 
 with open('output.csv', 'w') as f:
-    keys = ['title', 'flood', 'address', 'followInfo', 'tag', 'priceInfo']
+    keys = ['city', 'title', 'flood', 'address', 'followInfo', 'tag', 'priceInfo']
     writer = csv.DictWriter(f, fieldnames=keys)
     writer.writeheader()
     for unit in units:
